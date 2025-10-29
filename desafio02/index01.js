0b5fafa34cc94e7109e93ee100dd848de4215922
Luiz Antonio Ponciano Costa Bezerra <luiz.306022@alunosatc.edu.br>
2025-10-29 19:27:17 -0300

    desafio02

--- desafio02/index01.js
+++ desafio02/index01.js
@@ -1,1 +1,42 @@
-console.log("Hello, World! 001");
+class LegacyPaymentSystem {
+    makePayment(amount) {
+        console.log(`Pagando R$${amount} com sistema legado.`);
+    }
+}
+
+class ModernPaymentAPI {
+    process(amountInCents) {
+        console.log(`Pagamento de R$${amountInCents / 100} via API moderna.`);
+    }
+}
+
+// Adapter: conexão entre o makePayment e o process
+class ModernPaymentAdapter {
+    constructor(modernAPI) {
+        this.modernAPI = modernAPI;
+    }
+
+    makePayment(amount) {
+        const amountInCents = amount * 100;
+        this.modernAPI.process(amountInCents);
+    }
+}
+
+class PaymentProcessor {
+    constructor(system) {
+        this.system = system;
+    }
+
+    pay(amount) {
+        this.system.makePayment(amount);
+    }
+}
+
+const legacy = new LegacyPaymentSystem();
+const processorLegacy = new PaymentProcessor(legacy);
+processorLegacy.pay(100);
+
+const modernAPI = new ModernPaymentAPI();
+const modernAdapter = new ModernPaymentAdapter(modernAPI);
+const processorModern = new PaymentProcessor(modernAdapter);
+processorModern.pay(250);
--- desafio02/index02.js
+++ desafio02/index02.js
@@ -1,1 +1,40 @@
-console.log("Hello, World! 002");
+class TV {
+    turnOn() {
+        console.log("TV ligada.");
+    }
+    turnOff() {
+        console.log("TV desligada.");
+    }
+}
+
+class Radio {
+    turnOn() {
+        console.log("Rádio ligado.");
+    }
+    turnOff() {
+        console.log("Rádio desligado.");
+    }
+}
+
+class RemoteControl {
+    constructor(device) {
+        this.device = device;
+    }
+
+    pressPowerButton(on) {
+        if (on) this.device.turnOn();
+        else this.device.turnOff();
+    }
+}
+
+const tv = new TV();
+const remoteForTV = new RemoteControl(tv);
+remoteForTV.pressPowerButton(true);
+remoteForTV.pressPowerButton(false);
+
+console.log("----");
+
+const radio = new Radio();
+const remoteForRadio = new RemoteControl(radio);
+remoteForRadio.pressPowerButton(true);
+remoteForRadio.pressPowerButton(false);
